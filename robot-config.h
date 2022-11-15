58cce1092b68b46b3a3f2769c15f10ff6cbbb19b
BluPros <59413076+BluPros@users.noreply.github.com>
2022-11-15 15:43:32 -0500

    Create robot-config.h

---
+++ robot-config.h
@@ -0,0 +1,17 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern smartdrive Drivetrain;
+extern digital_out TranSolenoid;
+extern digital_out ShootSolenoid;
+extern motor_group Intake;
+extern motor_group Flywheel;
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void);
